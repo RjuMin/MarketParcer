c6d8b5fd7459f9b88df7d16bf18bc6b347ddd92b
RjuMin <mihailsilantev141@gmail.com>
2025-03-27 16:36:25 +0300

    Create ProductController.php
    Перед этим на Laravel необходимо:
    1. Установка Laravel проекта
    composer create-project laravel/laravel price-tracker
    cd price-tracker
    2. Создание контроллера для работы с товарами
    php artisan make:controller ProductController

---
+++ ProductController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class ProductController extends Controller
+{
+    public function getProduct($product_id)
+    {
+        // Тестовые данные (в реальной реализации будем обращаться к API Яндекс.Маркета)
+        $testProducts = [
+            '123' => [
+                'id' => '123',
+                'name' => 'Смартфон Xiaomi Redmi Note 10 Pro',
+                'price' => 24990,
+                'currency' => 'RUB',
+                'url' => 'https://market.yandex.ru/product/123',
+                'image' => 'https://example.com/image1.jpg',
+                'rating' => 4.5,
+                'updated_at' => now()->toDateTimeString()
+            ],
+            '456' => [
+                'id' => '456',
+                'name' => 'Наушники Apple AirPods Pro',
+                'price' => 18990,
+                'currency' => 'RUB',
+                'url' => 'https://market.yandex.ru/product/456',
+                'image' => 'https://example.com/image2.jpg',
+                'rating' => 4.8,
+                'updated_at' => now()->toDateTimeString()
+            ]
+        ];
+
+        if (array_key_exists($product_id, $testProducts)) {
+            return response()->json($testProducts[$product_id]);
+        }
+
+        return response()->json([
+            'error' => 'Product not found',
+            'message' => 'The requested product ID does not exist in our test database'
+        ], 404);
+    }
+}
